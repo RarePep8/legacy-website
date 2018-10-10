df6113455e71013f7a51261e0ce12fd8a105dba4
rarepep8 <alex.wong.utsc@gmail.com>
2018-10-09 22:14:58 -0400

    Added projects.html

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 var weekdayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 var dateObject = new Date();
 document.getElementById('date-text').innerHTML = weekdayArray[dateObject.getDay()];
-//if(dateObject.getDay() != 3){
-  document.getElementById("date-image").style.display='none';
-//}
 var showBorder = false;
 function clock(){
   var dynamicDateObject = new Date();
